bdd6c836c8cfbf28f327bd32ae6e92fcfe09bbff
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-09-11 17:16:25 +0700

    setting button "answers"

--- Quiz Game/Assets/Scripts/Quiz.cs
+++ Quiz Game/Assets/Scripts/Quiz.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -17,6 +18,46 @@ public class Quiz : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetNextQuestion();
+        // DisplayQuestion();
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+
+        Image buttonImage;
+
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponentInChildren<Image>();
+
+            buttonImage.sprite = correctAnswerSprite; // Image.Sprite: https://docs.unity3d.com/2019.1/Documentation/ScriptReference/UI.Image-sprite.html
+                                                      // Sprite: https://docs.unity3d.com/2019.1/Documentation/ScriptReference/Sprite.html
+        }
+
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+
+            questionText.text = "Sorry, the correct answer was: \n" + question.GetAnswer(correctAnswerIndex);
+
+            buttonImage = answerButtons[correctAnswerIndex].GetComponentInChildren<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+
+        SetButtonState(false);
+    }
+
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+
+    void DisplayQuestion()
+    {
         questionText.text = question.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++)
@@ -25,29 +66,22 @@ public class Quiz : MonoBehaviour
             buttonText.text = question.GetAnswer(i);
         }
     }
-    
-    public void OnAnswerSelected(int index)
+
+    void SetButtonState(bool state)
     {
-        
-        Image buttonImage;
-
-        if (index == question.GetCorrectAnswerIndex())
+        for (int i = 0; i < answerButtons.Length; i++)
         {
-            questionText.text = "Correct!";
-            buttonImage = answerButtons[index].GetComponentInChildren<Image>(); 
-
-            buttonImage.sprite = correctAnswerSprite; // Image.Sprite: https://docs.unity3d.com/2019.1/Documentation/ScriptReference/UI.Image-sprite.html
-                                                      // Sprite: https://docs.unity3d.com/2019.1/Documentation/ScriptReference/Sprite.html
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state; // interactable: https://docs.unity3d.com/2018.1/Documentation/ScriptReference/UI.Selectable-interactable.html
         }
+    }
 
-        else 
+    void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
         {
-            correctAnswerIndex = question.GetCorrectAnswerIndex();
-
-            questionText.text = "Sorry, the correct answer was: \n" + question.GetAnswer(correctAnswerIndex);
-
-            buttonImage = answerButtons[correctAnswerIndex].GetComponentInChildren<Image>(); 
-            buttonImage.sprite = correctAnswerSprite;
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
         }
     }
 }
