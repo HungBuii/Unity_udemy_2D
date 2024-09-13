a784c71325d7c087bed1bf9f9d14c05ba03fb30f
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-09-13 17:50:05 +0700

    update list questions and update timer

--- Quiz Game/Assets/Scripts/Quiz.cs
+++ Quiz Game/Assets/Scripts/Quiz.cs
@@ -4,30 +4,63 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Quiz : MonoBehaviour
 {
+    [Header("Questions")]
     [SerializeField] TextMeshProUGUI questionText;
-    [SerializeField] QuestionSO question;
-    [SerializeField] GameObject[] answerButtons;
+    [SerializeField] List<QuestionSO> questions = new List<QuestionSO>(); // new List<QuestionSO>(): may or may not be written because has "SerializeField"
+    QuestionSO currentQuestion;
 
+    [Header("Answers")]
+    [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
+    bool hasAnsweredEarly;
+
+    [Header("Button Colors")]
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
+
+    [Header("Timer")]
+    [SerializeField] Image timerImage;
+    Timer timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetNextQuestion();
-        // DisplayQuestion();
+        timer = FindObjectOfType<Timer>();
+    }
+
+    void Update()
+    {
+        timerImage.fillAmount = timer.fillFraction;
+        if (timer.loadNextQuestion)
+        {
+            hasAnsweredEarly = false;
+            GetNextQuestion();
+            timer.loadNextQuestion = false;
+        }
+        else if (!hasAnsweredEarly && !timer.isAnsweringQuestion)
+        {
+            DisplayAnswer(-1);
+            SetButtonState(false);
+        }
     }
 
     public void OnAnswerSelected(int index)
     {
+        hasAnsweredEarly = true;
+        DisplayAnswer(index);
+        SetButtonState(false);
+        timer.CancelTimer();
+    }
 
+    void DisplayAnswer(int index)
+    {
         Image buttonImage;
 
-        if (index == question.GetCorrectAnswerIndex())
+        if (index == currentQuestion.GetCorrectAnswerIndex())
         {
             questionText.text = "Correct!";
             buttonImage = answerButtons[index].GetComponentInChildren<Image>();
@@ -38,32 +71,48 @@ public class Quiz : MonoBehaviour
 
         else
         {
-            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            correctAnswerIndex = currentQuestion.GetCorrectAnswerIndex();
 
-            questionText.text = "Sorry, the correct answer was: \n" + question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was: \n" + currentQuestion.GetAnswer(correctAnswerIndex);
 
             buttonImage = answerButtons[correctAnswerIndex].GetComponentInChildren<Image>();
             buttonImage.sprite = correctAnswerSprite;
         }
-
-        SetButtonState(false);
     }
 
     void GetNextQuestion()
     {
-        SetButtonState(true);
-        SetDefaultButtonSprites();
-        DisplayQuestion();
+        if (questions.Count > 0)
+        {
+            SetButtonState(true);
+            SetDefaultButtonSprites();
+            GetRandomQuestion();
+            DisplayQuestion();
+        }
+    }
+
+    void GetRandomQuestion()
+    {
+        // int index = 0;
+        // currentQuestion = questions[index++];
+
+        int index = Random.Range(0, questions.Count);
+        currentQuestion = questions[index];
+
+        if (questions.Contains(currentQuestion))
+        {
+            questions.Remove(currentQuestion);
+        }
     }
 
     void DisplayQuestion()
     {
-        questionText.text = question.GetQuestion();
+        questionText.text = currentQuestion.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++)
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>(); // Button: https://docs.unity3d.com/2019.1/Documentation/ScriptReference/UI.Button.html
-            buttonText.text = question.GetAnswer(i);
+            buttonText.text = currentQuestion.GetAnswer(i);
         }
     }
 
--- Quiz Game/Assets/Scripts/Timer.cs
+++ Quiz Game/Assets/Scripts/Timer.cs
@@ -56,6 +56,6 @@ public class Timer : MonoBehaviour
             }
         }
 
-        Debug.Log(isAnsweringQuestion + ": " + timerValue + "= " + fillFraction);
+        // Debug.Log(isAnsweringQuestion + ": " + timerValue + "= " + fillFraction); // Using for testing fillFraction
     }
 }
